7f955855b579458a450273635c840fcd06396ae1
Arthur <arthur@netweightit.com>
2016-09-22 05:19:24 +0300

    homepage, footer

--- protected/views/layouts/front_bottom_footer.php
+++ protected/views/layouts/front_bottom_footer.php
@@ -1,31 +1,20 @@
 <!-- Footer ================================================== -->
 <footer>
     <div class="container">
-        <div class="row">
-            <div class="col-md-3 col-sm-2">
-  
-                <?php $this->widget('zii.widgets.CMenu', FunctionsV3::getFooterMenu1() );?> 
-                
-                <?php //FunctionsV3::getFooterAddress();?>
-                
-                
-            </div>
-            <div class="col-md-2 col-sm-2">
-                                
-                 <?php $this->widget('zii.widgets.CMenu', FunctionsV3::getFooterMenu2() );?> 
-
-            </div>
-            <div class="col-md-4 col-sm-4" id="newsletter">
+        <div class="row row-footer-links">
+            
+            
+            <div class="col-md-4 col-sm-4" id="newsletter" style="margin-right: 50px;">
 
                 <?php if ( getOptionA('disabled_subscription') == ""):?>
                 
-                    <h3>
+                    <h3 style="margin-top: 0px;">
                         Newsletter
                     </h3>
                     <p>
                         Join our newsletter to keep be informed about offers and news.
-                        <?php //echo t("Subscribe to our newsletter") ?>
                     </p>
+                    
                     <div id="message-newsletter_2">
                     </div>
                     
@@ -41,36 +30,132 @@
                             
                         </div>
                         <input type="submit" value=" <?php echo t("Subscribe")?>" class="btn_1" id="submit-newsletter_2">
+                        
                     </form>
                 
                 <?php endif;?>
-                
-               
-                
-                
+
+            </div>
+            
+            
+            <div class="col-md-4 col-sm-3">
+  
+                <?php $this->widget('zii.widgets.CMenu', FunctionsV3::getFooterMenu1() );?> 
                 
             </div>
+            <div class="col-md-3 col-sm-3">
+                                
+                 <?php $this->widget('zii.widgets.CMenu', FunctionsV3::getFooterMenu2() );?> 
+
+            </div>
+            
+            
+<!--
             
             
             <div class="col-md-3 col-sm-4">
                 
                
-            </div>
+            </div>-->
         </div><!-- End row -->
-        <div class="row">
-            <div class="col-md-4">
-                
-            </div>
-            <div class="col-md-4">
-                
-            </div>
-            <div class="col-md-4">
-                
-            </div>
-            <div class="col-md-4">
-                
-            </div>
+        
+        
+        
+        
+        
+
+<!--CUISINE SECTIONS-->
+<?php if ( $list = FunctionsV3::getCuisine() ): ?>
+
+<div class="row row-delivery-links">
+
+
+    <div class="col-md-12">
+        <h1>
+            Browse delivery restaurants
+        </h1>
+    </div>
+    <div class="col-md-8">
+        <h4>
+            Browse delivery & takeout by cuisines
+        </h4>
+        
+        <div class="col-md-6">
+        <ul>
+            <?php $x=1; ?>
+            <?php foreach ( array_slice($list, 20, 18) as $val): ?>
+            <!--<div class="col-md-6 col-sm-6 indent-5percent nopad">-->
+            <li>
+                <a href="<?php echo Yii::app()->createUrl('/store/cuisine',array("category"=>$val['cuisine_id']))?>" 
+               class="<?php echo ($x%2)?"even":'odd'?>">
+                <?php 
+                $cuisine_json['cuisine_name_trans']=!empty($val['cuisine_name_trans'])?json_decode($val['cuisine_name_trans'],true):'';	 
+                echo qTranslate($val['cuisine_name'],'cuisine_name',$cuisine_json);
+
+                ?>
+                </a>
+            </li> 
+            <!--</div>-->   
+            <?php $x++; ?>
+            <?php endforeach; ?>
+        </ul> 
         </div>
+        <div class="col-md-6">
+            <ul>
+                <?php $x=1; ?>
+                <?php foreach ( array_slice($list, 38, 18) as $val): ?>
+                <!--<div class="col-md-6 col-sm-6 indent-5percent nopad">-->
+                <li>
+                  <a href="<?php echo Yii::app()->createUrl('/store/cuisine',array("category"=>$val['cuisine_id']))?>" 
+                 class="<?php echo ($x%2)?"even":'odd'?>">
+                  <?php 
+                  $cuisine_json['cuisine_name_trans']=!empty($val['cuisine_name_trans'])?json_decode($val['cuisine_name_trans'],true):'';	 
+                  echo qTranslate($val['cuisine_name'],'cuisine_name',$cuisine_json);
+
+                  ?>
+                  </a>
+                </li> 
+                <!--</div>-->   
+                <?php $x++; ?>
+                <?php endforeach; ?>
+            </ul>       
+        </div>
+        
+    </div>
+
+
+    <div class="col-md-4">
+        <h4>
+            Browse by cities
+        </h4>
+        <ul>
+            <?php $x=1;?>
+            <?php foreach ( array_slice($list, 56) as $val): ?>
+
+            <li>
+                <a href="<?php echo Yii::app()->createUrl('/store/cuisine',array("category"=>$val['cuisine_id']))?>" 
+               class="<?php echo ($x%2)?"even":'odd'?>">
+                <?php 
+                $cuisine_json['cuisine_name_trans']=!empty($val['cuisine_name_trans'])?json_decode($val['cuisine_name_trans'],true):'';	 
+                echo qTranslate($val['cuisine_name'],'cuisine_name',$cuisine_json);
+
+                ?>
+                </a>
+            </li>  
+
+            <?php $x++;?>
+            <?php endforeach;?>
+
+            <li class="">
+                <a href="<?php echo Yii::app()->createUrl('/store/browse/' )?>" class="clean">
+                    Show more
+                </a>
+            </li>
+        </ul>
+    </div>
+
+<?php endif;?>
+
         <div class="row">
             <div class="col-md-12">
                 <div id="social_footer">
