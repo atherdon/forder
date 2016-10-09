72467484ceaeedd6585590b009525a4223ecbe16
Constantine <business@netweightit.com>
2016-10-09 00:42:43 -0700

    welcome mail

---
+++ protected/mail/welcome.php
@@ -0,0 +1,459 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <link href='http://fonts.googleapis.com/css?family=Lato:400,300,700|Open+Sans' rel='stylesheet' type='text/css'>
+        <title>
+            
+            
+            
+            
+            QuickFood
+        
+        
+        
+        
+        </title>
+        <style type="text/css">
+            .ExternalClass {
+                    width: 100%;
+                    background-color: #ffffff;
+            }
+
+            body {
+                    font-size: 14px;
+                    line-height: 1;
+                    background-color: #ffffff;
+                    margin: 0;
+                    padding: 0;
+                    -webkit-font-smoothing: antialiased;
+                    font-family: 'Lato', Arial, sans-serif;
+            }
+            #bodyTable {
+                    height: 100% !important;
+                    margin: 0;
+                    padding: 0;
+                    width: 100% !important;
+            }
+            table {
+                    border-collapse: collapse;
+                    mso-table-lspace: 0pt;
+                    mso-table-rspace: 0pt;
+            }
+            td, td a {
+                    color: #778598;
+                    text-decoration: none;
+            }
+            td {
+                    font-size: 14px;
+                    line-height: 1;
+            }
+            img {
+                    border: none;
+                    outline: none;
+                    text-decoration: none;
+                    display: inline-block;
+            }
+            p {
+                    margin: 0;
+                    padding: 0;
+            }
+            a:hover, td a:hover {
+                    color: #778598;
+                    outline: none;
+            }
+            .button:hover {
+                    background: #ffffff !important;
+            }
+            .btn:hover {
+                    background: #b2b2b2 !important;
+            }
+
+            .button:hover a {
+                    color: #06cfba !important;
+            }
+            .btn:hover a {
+                    color: #ffffff !important;
+            }
+
+            @media all and (max-width: 639px) {
+                    body[yahoo] .fitScreen {
+                            width: 100%;
+                    }
+                    body[yahoo] .left-image img {
+                            width: 100%;
+                            height: auto;
+                    }
+                    body[yahoo] .boxes-600 {
+                            width: 90% !important;
+                    }
+                    body[yahoo] .height-600 {
+                            height: 32px !important;
+                    }
+            }
+            @media all and (max-width: 599px) {
+
+                    body[yahoo] .menu-wrap {
+                            width: 96%;
+                    }
+                    body[yahoo] .image-480 {
+                            width: 100% !important;
+                            height: auto;
+                    }
+                    body[yahoo] .box-wrap {
+                            width: 92% !important;
+                    }
+                    body[yahoo] .width-90 {
+                            width: 90% !important;
+                    }
+                    body[yahoo] .height-24 {
+                            height: 24px !important;
+                    }
+                    body[yahoo] .height-600 {
+                            height: 20px !important;
+                    }
+                    body[yahoo] .width_6 {
+                            width: 6px !important;
+                    }
+                    body[yahoo] .width-220 {
+                            width: 220px !important;
+                    }
+                    body[yahoo]  .width-15 {
+                            width: 15px !important;
+                    }
+
+                    body[yahoo]  .width-25 {
+                            width: 25px !important;
+                    }
+
+            }
+            @media all and (max-width: 479px) {
+
+                    body[yahoo] .td-block {
+                            display: block !important;
+                    }
+                    body[yahoo] .position {
+                            text-align: center !important;
+                    }
+                    body[yahoo] .lineing {
+                            line-height: 40px !important;
+                    }
+                    body[yahoo] .height-mobile {
+                            height: 25px !important;
+                    }
+                    body[yahoo] .height-mobile-bt {
+                            height: 15px !important;
+                    }
+                    body[yahoo] .width-100 {
+                            width: 100% !important;
+                    }
+                    body[yahoo] .width-0 {
+                            width: 0% !important;
+                    }
+                    body[yahoo] .image-flat img {
+                            width: 90% !important;
+                            height: auto;
+                    }
+                    body[yahoo] .width-text {
+                            width: 92% !important;
+                    }
+                    body[yahoo] .width-95-mob {
+                            width: 95% !important;
+                    }
+                    body[yahoo] .width-15-mob {
+                            width: 15px !important;
+                    }
+                    body[yahoo] .position-align span {
+                            margin: 0 auto;
+                    }
+                    body[yahoo] .top-footer {
+                            width: 215px;
+                    }
+                    body[yahoo] .call-box {
+                            width: 100% !important;
+                            float: none;
+                            height: 30px;
+                    }
+
+                    body[yahoo] .hide {
+                            display: none !important;
+                    }
+                    body[yahoo] .width-220 {
+                            width: 100% !important;
+                            display: block;
+                    }
+                    body[yahoo] .spacer {
+                            display: block;
+                            width: 100% !important;
+                            height: 20px;
+                    }
+                    body[yahoo] .width-85 {
+                            width: 85% !important;
+                    }
+
+            }
+        </style>
+    </head>
+    <body yahoo="fix" style="background-color:#bfbfbf; margin: 0; padding: 0; font-family:'Lato',Arial, sans-serif;  line-height:1;color:#969696;" id="bodyTable">
+
+        <!-- 	Section-1 start here -->
+        <table width="640" border="0" cellpadding="0" cellspacing="0" align="center"  class="table_wrapper fitScreen" style="background: #ffffff">
+                <tr>
+                        <td height="36" class="height-mobile">
+
+
+
+
+
+                            <img src="images/blank.gif" alt="blank" width="1" height="1" />
+
+
+
+
+                        </td>
+                </tr>
+                <tr align="center">
+                        <td>
+                        <table cellpadding="0" cellspacing="0" border="0" align="center" class="menu-wrap" width="545">
+                                <tr>
+                                        <td class="td-block position" ><a href="#" style="font-family:'Lato',Arial, sans-serif; font-size: 24px; font-weight: 700; color:#828282;text-transform: uppercase; text-decoration: none;"> meno<span style="font-size: 30px; color:#06cfba;">.</span></a></td>
+                                        <td align="right" width="285" class="td-block">
+
+
+                                        </td>
+                                </tr>
+
+                        </table></td>
+                </tr>
+                <tr>
+                        <td height="35" class="height-mobile-bt">
+
+
+
+
+
+                            <img src="images/blank.gif" alt="blank" width="1" height="1" />
+
+
+
+
+
+                        </td>
+                </tr>
+        </table>
+        <!-- 	Section-1 End here -->
+
+
+
+
+
+
+
+
+        <!-- 	Section-10 start here -->
+
+        <table width="640" border="0" cellpadding="0" cellspacing="0" align="center"  class="table_wrapper fitScreen" style="background: #ffffff;">
+            <tr>
+                <td>
+                <table cellpadding="0" cellspacing="0" border="0" class="boxes" width="100%" >
+                        <tr>
+                                <td width="50" class="width-15-mob">
+
+
+
+
+
+
+
+                                    <img src="images/blank.gif" alt="blank" width="1" height="1" />
+
+
+
+
+
+
+                                </td>
+                                <td>
+                                <table cellpadding="0" cellspacing="0" border="0" class="boxes" width="95%">
+                                        <tr>
+                                                <td height="55">
+
+
+
+
+
+                                                    <img src="images/blank.gif" alt="blank" width="1" height="1" />
+
+
+
+
+
+
+                                                </td>
+                                        </tr>
+                                        <tr>
+                                                <td style="font-size: 14px; color:#737373; text-transform: uppercase;  letter-spacing: .5px; font-family:'Lato',Arial, sans-serif; font-weight: 700;"> one COLUMN </td>
+                                        </tr>
+                                        <tr>
+                                                <td height="16">
+
+
+
+
+
+
+
+                                                    <img src="images/blank.gif" alt="blank" width="1" height="1" />
+
+
+
+
+
+
+
+                                                </td>
+                                        </tr>
+                                        <tr>
+                                                <td height="5"><span style="width: 42px;  border-top:5px solid #83e7dd; display: block; "> </span></td>
+                                        </tr>
+                                        <tr>
+                                                <td height="25">
+
+
+
+
+
+
+                                                    <img src="images/blank.gif" alt="blank" width="1" height="1" />
+
+
+
+
+
+
+
+                                                </td>
+                                        </tr>
+                                        <tr>
+                                                <td style="font-size: 14px; line-height: 1.5; color:#737373;  letter-spacing: .5px; font-family:'Lato',Arial, sans-serif; font-weight: 300;"> Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. </td>
+                                        </tr>
+                                        <tr>
+                                                <td height="72">
+
+
+
+
+
+
+
+
+                                                    <img src="images/blank.gif" alt="blank" width="1" height="1" />
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                </td>
+                                        </tr>
+
+                                </table></td>
+
+                        </tr>
+
+                </table></td>
+            </tr>
+        </table>
+        <!-- 	Section-10 End here -->
+
+
+
+
+
+
+        <!-- 	Section-13 Start here -->
+
+        <table width="640" border="0" cellpadding="0" cellspacing="0" align="center"  class="table_wrapper fitScreen" style="background:#1abaa9;">
+
+            <tr>
+                    <td height="35">
+
+
+
+
+
+
+
+
+
+                        <img src="images/blank.gif" alt="blank" width="1" height="1" />
+
+
+
+
+
+
+
+
+
+
+
+                    </td>
+            </tr>
+            <tr>
+                    <td align="center">
+                    <table cellpadding="0" cellspacing="0" border="0" class="width-text" width="80%" align="center" >
+
+                            <tr>
+                                    <td>
+                                    <table cellpadding="0" cellspacing="0" border="0" class="" width="40%" align="left"  >
+                                            <tr>
+                                                    <td style="font-size: 12px; color:#ffffff; font-family:'Lato',Arial, sans-serif; word-spacing: 3px; "> Copyright  &copy;  Meno </td>
+                                            </tr>
+                                    </table>
+                                    <table cellpadding="0" cellspacing="0" border="0" class=""  width="150" align="right" >
+                                            <tr>
+                                                    <td></td>
+                                                    <td width="1"></td>
+                                                    <td align="right">
+                                                                    <a href="#" style="font-size: 12px; display: block; color:#ffffff; font-family:'Lato',Arial, sans-serif; letter-spacing: .5px; text-decoration: none; "> unsubscribe </a>
+                                                    </td>
+                                            </tr>
+                                    </table></td>
+
+                            </tr>
+                    </table></td>
+            </tr>
+
+            <tr>
+                    <td height="35">
+
+
+
+
+
+
+                        <img src="images/blank.gif" alt="blank" width="1" height="1" />
+
+
+
+
+
+
+
+
+                    </td>
+            </tr>
+
+        </table>
+
+    </body>
+</html>
